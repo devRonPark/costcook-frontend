2d194bdd9cc9e6356f1e02e9a70ff7e6ef6dee19
Byungchan Park <57528803+devRonPark@users.noreply.github.com>
2024-10-15 14:14:54 +0900

    Feat: 프로젝트 폴더 구조 설정 및 레이아웃 구성 컴포넌트 생성

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import DefaultLayout from "../components/layout/DefaultLayout";
+
+const Home = () => {
+  return (
+    <DefaultLayout>
+      <h1>홈</h1>
+    </DefaultLayout>
+  );
+};
+
+export default Home;
