db6bbd404f4f0f062d8f65ee770e0f03c2819b69
한민혁 <88497297+hazziu@users.noreply.github.com>
2024-10-17 18:30:12 +0900

    Feat : 메인 페이지 스타일링

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,12 +2,10 @@ import React, { useState } from 'react';
 import styled from 'styled-components';
 import { Button } from '@mui/material';
 import { Link } from 'react-router-dom';
-import RoundedButton from '../components/common/Button/RoundedButton';
-import RoundedButtonHalfWidth from '../components/common/Button/RoundedButtonHalfWidth';
 import Layout from '../components/layout/Layout';
 
 const HomePage = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false); // 모달 상태
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const openModal = () => setIsModalOpen(true);
   const closeModal = () => setIsModalOpen(false);
@@ -22,40 +20,69 @@ const HomePage = () => {
         </MoneyContainerWrapper>
       </SettingContainer>
       <RecommendContainer>
-        <RecommendListContainer>
-          {/* 추천을 받기 전에는 버튼으로 출력되게 구현 */}
-          <Link to="../Recommend">
-            <Button>추천하기</Button>
-          </Link>
+        <ListContainer>
+          <List>
+            <ReceiptImage>
+              <Link to="../Recommend">
+                <Button>추천하기</Button>
+              </Link>
+            </ReceiptImage>
+            <TextBox>
+              <TitleText>김치볶음밥</TitleText>
+              <PriceText>4300원</PriceText>
+              <StarText>★★★★☆ 4.0</StarText>
+            </TextBox>
+          </List>
 
-          {/* 추천을 받은 후에는 리스트가 출력되게 구현 */}
-          <RecommendList>
-            <h3>추천 리스트</h3>
-          </RecommendList>
-          <RecommendList>
-            <h3>추천 리스트</h3>
-          </RecommendList>
-          <RecommendList>
-            <h3>추천 리스트</h3>
-          </RecommendList>
-        </RecommendListContainer>
+          <List>
+            <ReceiptImage>이미지</ReceiptImage>
+            <TextBox>
+              <TitleText>김치볶음밥</TitleText>
+              <PriceText>4300원</PriceText>
+              <StarText>★★★★☆ 4.0</StarText>
+            </TextBox>
+          </List>
+          <List>
+            <ReceiptImage>이미지</ReceiptImage>
+            <TextBox>
+              <TitleText>김치볶음밥</TitleText>
+              <PriceText>4300원</PriceText>
+              <StarText>★★★★☆ 4.0</StarText>
+            </TextBox>
+          </List>
+        </ListContainer>
       </RecommendContainer>
       <UpcommingReceiptContainer>
         <UpcommingReceiptHeader>
           <h3>인기레시피</h3>
           <RightText href="#">더보기</RightText>
         </UpcommingReceiptHeader>
-        <UpcommingReceiptListContainer>
-          <UpcommingReceiptList>
-            <h2>리스트</h2>
-          </UpcommingReceiptList>
-          <UpcommingReceiptList>
-            <h2>리스트</h2>
-          </UpcommingReceiptList>
-          <UpcommingReceiptList>
-            <h2>리스트</h2>
-          </UpcommingReceiptList>
-        </UpcommingReceiptListContainer>
+        <ListContainer>
+          <List>
+            <ReceiptImage>이미지</ReceiptImage>
+            <TextBox>
+              <TitleText>김치볶음밥</TitleText>
+              <PriceText>4300원</PriceText>
+              <StarText>★★★★☆ 4.0</StarText>
+            </TextBox>
+          </List>
+          <List>
+            <ReceiptImage>이미지</ReceiptImage>
+            <TextBox>
+              <TitleText>김치볶음밥</TitleText>
+              <PriceText>4300원</PriceText>
+              <StarText>★★★★☆ 4.0</StarText>
+            </TextBox>
+          </List>
+          <List>
+            <ReceiptImage>이미지</ReceiptImage>
+            <TextBox>
+              <TitleText>김치볶음밥</TitleText>
+              <PriceText>4300원</PriceText>
+              <StarText>★★★★☆ 4.0</StarText>
+            </TextBox>
+          </List>
+        </ListContainer>
       </UpcommingReceiptContainer>
     </Layout>
   );
@@ -65,8 +92,6 @@ export default HomePage;
 
 const SettingContainer = styled.div`
   width: 100%;
-  flex-direction: column;
-  justify-content: space-between;
   border: 1px black solid;
 `;
 
@@ -97,28 +122,9 @@ const RecommendContainer = styled.div`
   width: 100%;
   border: 1px solid black;
   text-align: center;
-  display: flex; // Flexbox를 사용
-  flex-direction: column; // 세로 방향으로 정렬
-  justify-content: center; // 세로 중앙 정렬
-`;
-const RecommendListContainer = styled.div`
-  padding: 10px;
-  width: 100%;
   display: flex;
-  justify-content: space-between;
-  align-items: center;
-  border: 1px solid black;
-`;
-
-const RecommendList = styled.div`
-  display: flex;
+  flex-direction: column;
   justify-content: center;
-  align-items: center;
-  margin: 20px;
-  border: 1px solid black;
-  height: 220px;
-  width: 120px;
-  column-gap: 5px;
 `;
 
 const UpcommingReceiptContainer = styled.div`
@@ -129,7 +135,14 @@ const UpcommingReceiptContainer = styled.div`
   align-items: flex-start;
 `;
 
-const UpcommingReceiptListContainer = styled.div`
+const UpcommingReceiptHeader = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const ListContainer = styled.div`
   padding: 10px;
   width: 100%;
   display: flex;
@@ -138,15 +151,14 @@ const UpcommingReceiptListContainer = styled.div`
   border: 1px solid black;
 `;
 
-const UpcommingReceiptList = styled.div`
+const List = styled.div`
   display: flex;
-  justify-content: center;
+  flex-direction: column;
   align-items: center;
   margin: 20px;
   border: 1px solid black;
   height: 220px;
   width: 120px;
-  column-gap: 5px;
 `;
 
 const RightText = styled.a`
@@ -155,9 +167,32 @@ const RightText = styled.a`
   text-align: right;
 `;
 
-const UpcommingReceiptHeader = styled.div`
-  width: 100%;
+const ReceiptImage = styled.div`
+  height: 120px;
+  width: 120px;
+  border-bottom: 1px black solid;
   display: flex;
-  justify-content: space-between;
   align-items: center;
+  justify-content: center;
+`;
+
+const TextBox = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 3px 5px;
+  text-align: left;
+`;
+
+const TitleText = styled.h3`
+  margin: 3px 0;
+`;
+
+const PriceText = styled.a`
+  margin: 3px 0;
+`;
+
+const StarText = styled.a`
+  margin: 3px 0;
 `;
