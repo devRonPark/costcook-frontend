3f86bf5ccd9f2f4c5a366a46ade45a01fb88a416
한민혁 <88497297+hazziu@users.noreply.github.com>
2024-10-17 16:12:02 +0900

    Feat: 기본 레이아웃 들어가는 컴포넌트 스타일링

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -2,12 +2,13 @@ import styled from 'styled-components';
 import HomeIcon from '@mui/icons-material/Home'; // MUI Home 아이콘
 import SearchIcon from '@mui/icons-material/Search'; // MUI Search 아이콘
 import { Link } from 'react-router-dom';
+import BackButton from '../common/Button/BackButton';
 
 const Header = () => (
   <HeaderContainer>
     <IconWrapper>
       <Link to="/">
-        <HomeIconStyled />
+        <BackButton />
       </Link>
     </IconWrapper>
     <IconWrapper>
@@ -22,7 +23,7 @@ export default Header;
 
 const HeaderContainer = styled.header`
   box-sizing: border-box;
-  flex: 1; /* 1 part of the total height */
+  height: 65px;
   width: 100%;
   padding: 0 16px;
   display: flex;
@@ -33,7 +34,7 @@ const HeaderContainer = styled.header`
 
 const IconWrapper = styled.div`
   cursor: pointer;
-  font-size: 24px; /* 아이콘 크기 조정 */
+  font-size: 32px; /* 아이콘 크기 조정 */
   color: #333; /* 아이콘 색상 */
   &:hover {
     color: #007bff; /* 호버 시 색상 변경 */
@@ -41,11 +42,11 @@ const IconWrapper = styled.div`
 `;
 
 const HomeIconStyled = styled(HomeIcon)`
-  width: 24px; /* 아이콘 크기 조정 */
-  height: 24px; /* 아이콘 크기 조정 */
+  /* 민혁: 아이콘 크기 수정 시, 기본 크기 덮어써야함 (width, height 동작 안함) */
+  font-size: 32px !important;
 `;
 
 const SearchIconStyled = styled(SearchIcon)`
-  width: 24px; /* 아이콘 크기 조정 */
-  height: 24px; /* 아이콘 크기 조정 */
+  /* 민혁: 아이콘 크기 수정 시, 기본 크기 덮어써야함 (width, height 동작 안함) */
+  font-size: 32px !important;
 `;
