71810b81a4f0de2b33f825a2cbe03ace9362b83f
한민혁 <88497297+hazziu@users.noreply.github.com>
2024-10-21 11:53:11 +0900

    Feat : 각종 설정 변경

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import PreLoginPage from './pages/PreLoginPage';
 import OAuthVerification from './pages/OAuthVerification';
 import { AuthProvider } from './context/Auth/AuthProvider';
 import RecommendPage from './pages/RecommendPage';
+import RecipeDetail from './pages/RecipeDetail';
 
 function App() {
   return (
@@ -26,6 +27,7 @@ function App() {
         <Route path="/login" element={<LoginPage />} />
         <Route path="/home" element={<HomePage />} />
         <Route path="/recipe" element={<RecipePage />} />
+        <Route path="/recipeDetail" element={<RecipeDetail />} />
         <Route path="/recommend" element={<RecommendPage />} />
         <Route path="/budget" element={<BudgetPage />} />
         <Route path="/favorite" element={<FavoritePage />} />
