a3736b85ba455314129a0c4ee4ad1f0dafb24873
Byungchan Park <57528803+devRonPark@users.noreply.github.com>
2024-10-22 14:20:35 +0900

    Refactor: signUpOrLogin 함수명 변경

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from 'react';
 import './App.css';
 import {
   BrowserRouter as Router,
@@ -26,13 +27,16 @@ import AdminRecipePage from './pages/admin/RecipePage';
 import AdminIngredientPage from './pages/admin/IngredientPage';
 import PageTransition from './components/common/PageTransition';
 import RecipeIngredientPage from './pages/admin/RecipeIngredientPage';
+import { useAuth } from './context/Auth/AuthContext';
+import UserInfo from './pages/UserInfo';
 
 function App() {
   const location = useLocation();
-
   return (
-    <>
-      <ToastContainer />
+    <AuthProvider>
+      <ToastContainer
+        position="bottom-center" // 위치 설정
+      />
       <GlobalStyle /> {/* 전역 스타일 적용 */}
       <AnimatePresence mode="wait">
         <Routes location={location} key={location.pathname}>
@@ -116,24 +120,18 @@ function App() {
               </PageTransition>
             }
           />
-          <Route
-            path="/oauth/:provider"
-            element={
-              <AuthProvider>
-                <OAuthVerification />
-              </AuthProvider>
-            }
-          />
+          <Route path="/oauth/:provider" element={<OAuthVerification />} />
           <Route path="/admin/ingredient" element={<AdminIngredientPage />} />
           <Route path="/admin/recipe" element={<AdminRecipePage />} />
           <Route
             path="/admin/recipeIngredient"
             element={<RecipeIngredientPage />}
           />
+          <Route path="/users/me" element={<UserInfo />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </AnimatePresence>
-    </>
+    </AuthProvider>
   );
 }
 
