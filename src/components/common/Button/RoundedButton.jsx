6366b48622ef9c4261ec68fe56f34f0ac1180d89
Byungchan Park <57528803+devRonPark@users.noreply.github.com>
2024-10-25 11:01:55 +0900

    Feat: RoundedButton 재사용성 높이기 위해 width, border prop 추가

--- src/components/common/Button/RoundedButton.jsx
+++ src/components/common/Button/RoundedButton.jsx
@@ -5,26 +5,30 @@ const RoundedButton = ({
   onClick = () => (window.location.href = '#'), // 기본값: 페이지 이동 없음
   backgroundColor = 'white', // 기본값: 흰색 배경
   hoverBackgroundColor = '#e0a44b', // 기본값: hover 시 배경색
+  width = '400px', // 기본값: 400px
+  border = '1px solid black', // 기본값: 1px 실선 검정색
 }) => {
   return (
-    <MainContainer
+    <RoundedButtonContainer
       onClick={onClick}
       backgroundColor={backgroundColor}
       hoverBackgroundColor={hoverBackgroundColor}
+      width={width}
+      border={border}
     >
       {text}
-    </MainContainer>
+    </RoundedButtonContainer>
   );
 };
 
 export default RoundedButton;
 
-const MainContainer = styled.button`
-  width: 400px;
+const RoundedButtonContainer = styled.button`
+  width: ${(props) => props.width}; // 가로 길이 설정
   height: 60px;
   font-size: 20px;
   background-color: ${(props) => props.backgroundColor}; // 배경색 설정
-  border: 1px solid black;
+  border: ${(props) => props.border}; // 테두리 설정
   padding: 10px;
   border-radius: 20px;
   cursor: pointer;
