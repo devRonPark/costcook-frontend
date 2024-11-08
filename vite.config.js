3d49e2122a0625c0a8d7c59ca3facb48d5d36015
Byungchan Park <57528803+devRonPark@users.noreply.github.com>
2024-11-08 17:26:16 +0900

    fix: 빌드 결과물 build 폴더에 생성하도록 수정

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
   server: {
     port: 3000,
+  },
+  build: {
+    outDir: 'build', // 빌드 결과물을 'build' 폴더에 생성
   },
 });
